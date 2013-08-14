063175cb4befa21223829433a5a961d8db98a95c
montekaka <josh.chen.jia@gmail.com>
2013-08-13 23:31:40 -0700

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Omrails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Set up Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Omrails::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
