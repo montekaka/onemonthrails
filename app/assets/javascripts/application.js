12fdc17f6d5b900bfbb12c2bf4cf0e1b36eea9f9
montekaka <josh.chen.jia@gmail.com>
2013-08-18 21:05:05 -0700

    Add JQuery Masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,5 +6,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.masonry.min.js
 //= require bootstrap
 //= require_tree .
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,8 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    #@pins = Pin.all
+    @pins = Pin.order("created_at desc")
 
     # only current user can see his own pins
     #@pins = current_user.pins.all
